66081c4385d1d19382c22e144bb39248f626f51e
sumanahuja <54740525+sumanahuja@users.noreply.github.com>
2022-11-04 13:45:42 +1300

    stage 1 kotlin project encryption

--- Main.kt
+++ Main.kt
@@ -1,25 +1,52 @@
 package encryptdecrypt
 
 fun main() {
-    val str = readln().toString()
-    val key = readln().toInt()
-    val myList = mutableListOf<Char>()
-    var sum = 0
-   // print(('z'.code + 5 -97) / 26 + 97 )
-    for( i in str.indices) {
-        if (str[i].code in 97..122 || str[i] == ' ') {
-            if (str[i] == ' ') {
-                myList.add(' ')
-                //print("space")
-            } else {
-                sum = (str[i].code + key - 97) % 26 + 97
-                myList.add((sum).toChar())
-                //println(sum.toChar())
-            }
-
+    val str = "we found a treasure!"
+    var newstr=""
+    val strLength = str.length
+    for (i in 0..strLength-1)
+    {
+        if(str[i] == 'w') {
+            newstr = newstr + "d"
+        }
+        if(str[i] == 'e') {
+            newstr = newstr+"v"
+        }
+        if(str[i] == 'f') {
+            newstr = newstr+"u"
+        }
+        if(str[i] == 'o') {
+            newstr = newstr+"l"
+        }
+        if(str[i] == 'u') {
+            newstr = newstr+"f"
+        }
+        if(str[i] == 'n') {
+            newstr = newstr+"m"
+        }
+        if(str[i] == 'd') {
+            newstr = newstr+"w"
+        }
+        if(str[i] == ' ') {
+            newstr = newstr+" "
+        }
+        if(str[i] == 'a') {
+            newstr += "z"
+        }
+        if(str[i] == 't') {
+            newstr = newstr+"g"
+        }
+        if(str[i] == 'r') {
+            newstr = newstr+"i"
+        }
+        if(str[i] == 's') {
+            newstr = newstr+"h"
+        }
+        if(str[i] == '!') {
+            newstr = newstr+"!"
         }
     }
-    //println(myList.toString())
-    println(myList.joinToString().replace(", ",""))
+
+    println(newstr)
 
 }
